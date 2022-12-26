ab8954e24883f1e55155f577f1779a1d7a7d9c40
Sergionx <sergio180203@gmail.com>
2022-12-26 17:04:42 -0400

    helper function to read  inputs

---
+++ 2022/helpers/files.py
@@ -0,0 +1,6 @@
+from io import TextIOWrapper
+import os
+def readAdventInput(year: str, day: str) -> str:
+    with open(os.path.join(os.getcwd(), f"{year}\day {day}\input.txt"), "r") as f:
+        return f.read()
+        
