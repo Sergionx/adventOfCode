baff1a724b1b422781606b0e5364565e7a37a81f
Sergionx <sergio180203@gmail.com>
2023-01-02 14:30:20 -0400

    day 04 Second part

--- 2022/day 04/main.py
+++ 2022/day 04/main.py
@@ -11,7 +11,8 @@ def main():
     txt = readAdventInput("2022", "04")
     lines = txt.splitlines()
 
-    totalOverlaps = 0
+    fullOverlaps = 0
+    semiOverlaps = 0
     for line in lines:
         firstPair, secondPair = line.split(",")
 
@@ -21,11 +22,21 @@ def main():
         print(firstRange, secondRange)
 
         if firstRange[0] <= secondRange[0] and firstRange[1] >= secondRange[1]:
-            print("Overlap")
-            totalOverlaps += 1
+            print("Full Overlap")
+            fullOverlaps += 1
         elif secondRange[0] <= firstRange[0] and secondRange[1] >= firstRange[1]:
-            print("Overlap")
-            totalOverlaps += 1
+            print("Full Overlap")
+            fullOverlaps += 1
+        #Calculating no overlap is more easy than calculating any overlap,
+        #so if we calculater that, then the rest is semi overlap
+        elif firstRange[1] < secondRange[0] or secondRange[1] < firstRange[0]:
+            print("No Overlap")
+        else:
+            print("Semi Overlap")
+            semiOverlaps += 1
 
-    print(f"Total Overlaps: {totalOverlaps}")
+
+    print(f"Full Overlaps: {fullOverlaps}")
+    print(f"Semi Overlaps: {semiOverlaps}")
+    print(f"Total Overlaps: {fullOverlaps + semiOverlaps}")
 main()
