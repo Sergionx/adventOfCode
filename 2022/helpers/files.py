a36b0aa345cd25de35d2fd4e152c12a4af6571e8
Sergionx <sergio180203@gmail.com>
2023-01-02 14:31:07 -0400

    new helper to read input tests

--- 2022/helpers/files.py
+++ 2022/helpers/files.py
@@ -1,5 +1,10 @@
 from io import TextIOWrapper
 import os
+
 def readAdventInput(year: str, day: str) -> str:
     with open(os.path.join(os.getcwd(), f"{year}\day {day}\input.txt"), "r") as f:
+        return f.read()
+
+def readAdventTestInput(year: str, day: str) -> str:
+    with open(os.path.join(os.getcwd(), f"{year}\day {day}\input-test.txt"), "r") as f:
         return f.read()
