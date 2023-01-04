84278a51ed8eee8dda5494d5817472f3c6d305aa
Sergionx <sergio180203@gmail.com>
2023-01-04 00:18:38 -0400

    day 06 Part 2

--- 2022/day 06/main.py
+++ 2022/day 06/main.py
@@ -10,16 +10,31 @@ from helpers.files import readAdventInput, readAdventTestInput
 def main():
     txt = readAdventInput("2022", "06").strip()
     packetMarker = findPacketMarker(txt)
-    
+    messageMarker = findeMessageMarker(txt)
+
     print(f"The start of the packet is at index: {packetMarker}")
+    print(f"The start of the message is at index: {messageMarker}")
 
 def findPacketMarker(txt: str) -> int:
     for i in range(0, len(txt) - 3):
         char1, char2, char3, char4 = txt[i], txt[i+1], txt[i+2], txt[i+3]
-        if checkAllDifferent(char1, char2, char3, char4):
+        if checkAllDifferentWordy(char1, char2, char3, char4):
             return i + 4 
 
-def checkAllDifferent(char1: str, char2: str, char3: str, char4: str) -> bool:
+def findeMessageMarker(txt: str) -> int:
+    for i in range(0, len(txt) - 13):
+        listChars = [i for i in txt[i:i+14]]
+        if checkAllDifferent(listChars):
+            return i + 14
+
+def checkAllDifferent(listChars: list[any]) -> bool:
+    for i in range(0, len(listChars) - 1):
+        for j in range(i + 1, len(listChars)):
+            if listChars[i] == listChars[j]:
+                return False
+    return True
+
+def checkAllDifferentWordy(char1: str, char2: str, char3: str, char4: str) -> bool:
     if char1 != char2 and char1 != char3 and char1 != char4:
         if char2 != char3 and char2 != char4:
                 if char3 != char4:
